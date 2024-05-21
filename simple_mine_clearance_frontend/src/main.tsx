68f77d6072274ea9fcc235d01b78d30949e49453
Zcy <zcy_chao1024@163.com>
2024-05-21 22:34:41 +0800

    Pure interface without on-chain transactions

--- simple_mine_clearance_frontend/src/main.tsx
+++ simple_mine_clearance_frontend/src/main.tsx
@@ -4,24 +4,8 @@ import App from './App.tsx'
 
 import '@mysten/dapp-kit/dist/index.css';
 
-import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
-import { getFullnodeUrl } from '@mysten/sui.js/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-const networks = {
-	testnet: { url: getFullnodeUrl('testnet') },
-	mainnet: { url: getFullnodeUrl('mainnet') },
-};
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<SuiClientProvider networks={networks} defaultNetwork="testnet">
-				<WalletProvider>
-					<App />
-				</WalletProvider>
-			</SuiClientProvider>
-		</QueryClientProvider>
-	</React.StrictMode>,
+		<App />
+	</React.StrictMode>
 );
